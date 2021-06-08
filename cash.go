f757fe5d322126c420ad104dc6f5a7233656c14c
noahway <71214444+noahway@users.noreply.github.com>
2021-06-08 14:38:35 +0300

    Update cash.go
    Nojus Vaicekonis

--- cash.go
+++ cash.go
@@ -1,6 +1,38 @@
 package main
 import "fmt"
 
+
 func main() {
-    fmt.Println("Hello World")
+    
+    var f float64
+
+         fmt.Println("Enter a float value : ")
+         _, err := fmt.Scanf("%f", &f)
+
+         if err != nil {
+            fmt.Println("not number")
+         }
+         
+    sum := f*100
+    var counter int
+    counter = 0
+    	for sum >= 25 {
+    		counter += 1
+    		sum += -25
+    	}
+    	for sum >= 10 {
+    		counter += 1
+    		sum += -10
+    	}
+    	for sum >= 5 {
+    		counter += 1
+    		sum += -5
+    	}
+    	for sum >= 1 {
+    		counter += 1
+    		sum += -1
+    	}
+    	
+    	fmt.Println(counter)
+
 }
