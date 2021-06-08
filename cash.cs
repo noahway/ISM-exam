19f40ccf798f2e1fa1e2cc50349ce3c679c36255
noahway <71214444+noahway@users.noreply.github.com>
2021-06-08 14:08:16 +0300

    Update cash.cs
    Nojus Vaicekonis

--- cash.cs
+++ cash.cs
@@ -1,6 +1,39 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    Console.WriteLine("Enter sum of money");
+    float floatNumber = 0.0f;
+    int intNumber;      
+        // use of Single.Parse() Method
+        floatNumber = float.Parse(Console.ReadLine());
+    intNumber = (int)Math.Round(floatNumber*100);
+    
+    int i = intNumber;
+    int counter = 0;
+    while (i >= 25) 
+    {
+      i = i - 25;
+      counter++;
+    }
+    
+    while (i >= 10) 
+    {
+      i = i - 10;
+      counter++;
+    }
+    
+    while (i >= 5) 
+    {
+      i = i - 5;
+      counter++;
+    }
+    
+    while (i == 1) 
+    {
+      counter++;
+      i = i - 1;
+    }
+
+    Console.WriteLine(counter);
   }
 }
